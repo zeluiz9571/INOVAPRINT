eeaaba9be52fefa9853f4cb1fae7bce8e5ee7661
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-12-01 14:05:33 -0300

    sdfg

---
+++ codigo/sobre.php
@@ -0,0 +1,37 @@
+<?php
+    include_once('header.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="sobre.css">
+
+</head>
+<body>
+    <div class="title">
+        <h1 id="title-a">SOBRE:<h1>
+    </div>
+    <div class="content">          
+            <p> Adentre o mundo da inovação com a INOVAPRINT, o seu portal para liberar a imaginação! Aqui, as impressoras 3D transformam sua casa em um estúdio de criação, onde suas ideias ganham forma e vida.</p>
+
+<p>Na INOVAPRINT, acreditamos na liberdade total da expressão criativa. Nossas impressionantes impressoras 3D são o seu bilhete para dar vida a projetos simples ou complexos, de forma fácil e precisa.</p>
+
+<p>Cada modelo de impressora da INOVAPRINT é cuidadosamente selecionado, garantindo qualidade excepcional e simplicidade de uso. Queremos que cada momento na sua jornada de impressão 3D seja uma aventura emocionante.</p>
+
+<p>Com uma ampla gama de materiais à disposição, cada peça impressa é detalhada e resistente, destacando-se com qualidade impressionante.</p>
+
+<p>Valorizamos sua visão única. Nossa equipe está pronta para ajudar em cada etapa, desde a escolha do equipamento ideal até dicas avançadas de design, assegurando que cada projeto seja uma representação autêntica do seu talento.</p>
+
+<p>Venha explorar conosco esse universo onde a criatividade ganha vida. Transforme suas ideias em realidade, descubra um mundo de possibilidades e mergulhe na arte da impressão 3D com a INOVAPRINT.</P>
+
+<p>Faça parte dessa revolução criativa na impressão 3D com a INOVAPRINT. O futuro das suas criações está prestes a se desdobrar!
+            </p>
+    </div>
+</body>
+</html>
+<?php
+    include_once("footer.php")
+?>
