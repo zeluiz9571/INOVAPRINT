b8a8fb58e3d32eaa56a8d67ee8171d932d9ee4ae
ryan <ryan.nascimento257@gmail.com>
2023-12-01 11:41:43 -0300

    commit

---
+++ codigo/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+
+</head>
+<body>
+    <div class="main-div">
+        <div class="images-div">
+
+        </div>
+        <div class="login-div">
+            <img id='login-logo' src="../img/logo.png">
+            <h1>LOGIN</h1>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- -------------------------------------------------- -->
+<?php
+include('footer.php')
+?>
