1e2fa54b0fceff2b3c5ec2f2ee272a454f007ac9
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-12-07 11:20:21 -0300

    z

--- codigo/index.php
+++ codigo/index.php
@@ -18,13 +18,13 @@
 <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="../img/Back 1.png" class="d-block w-100" alt="...">
+      <img src="../img/wall6.jpeg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="../img/daicw.jpg" class="d-block w-100" alt="...">
+      <img src="../img/wall3.jpeg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="../img/carrossel3.png" class="d-block w-100" alt="...">
+      <img src="../img/wall4.jpeg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
