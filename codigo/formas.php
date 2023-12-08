88ce016c6327064d061f589a1d41a5ff4c585fa0
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-12-08 11:15:42 -0300

    sd

--- codigo/formas.php
+++ codigo/formas.php
@@ -13,43 +13,45 @@
 <body>
     <br>
     <div class="title" align="center">
-        <h3>Formas de pagamento</h3>
+        <h3>Formas de pagamento:</h3>
     </div>
     <div class="content">
-        <br>
-        <p>Cartão(crédito e débito)</p>
-        <br>
-        <div class="input-box">
-            <label for="number">Número do cartão</label>
-            <input id="number" name="number" placeholder="Número do cartão" required>
+        <div class="forma">
+            <br>
+            <p align="center">Cartão(crédito e débito)</p>
+            <br>
+                <div class="input-box">
+                    <label for="number">Número do cartão</label>
+                    <input id="number" name="number" placeholder="Número do cartão" required>
+                </div>
+                <br>
+                <div class="input-box">
+                    <label for="date">Data de expiração</label>
+                    <input id="number" name="number" placeholder="MM/DD/YYYY" required>
+                    <label for="CVC">CVC</label>
+                    <input id="number" name="CVC" placeholder="***" required>
+                </div>
+                <br>
+                <div class="input-box">
+                    <label for="name">Nome do titular</label>
+                    <input id="number" name="name" placeholder="Nome" required>
+                </div>
+                <br>
+                <div class="input-box">
+                    <label for="boleto">Boleto</label>
+                    <input id="number" name="boleto" placeholder="*******" required>
+                </div>
+                <br>
+                <div class="input-box">
+                    <label for="PIX">PIX</label>
+                    <input id="number" name="PIX" placeholder="*******" required>
+                </div>
         </div>
-        <br>
-        <div class="input-box">
-            <label for="date">Data de expiração</label>
-            <input id="number" name="number" placeholder="MM/DD/YYYY" required>
-            <label for="CVC">CVC</label>
-            <input id="CVC" name="CVC" placeholder="***" required>
-        </div>
-        <br>
-        <div class="input-box">
-            <label for="name">Nome do titular</label>
-            <input id="name" name="name" placeholder="Nome" required>
-        </div>
-        <br>
-        <div class="input-box">
-            <label for="boleto">Boleto</label>
-            <input id="boleto" name="name" placeholder="*******" required>
-        </div>
-        <br>
-        <div class="input-box">
-            <label for="PIX">PIX</label>
-            <input id="PIX" name="PIX" placeholder="*******" required>
-        </div>
-        <br>
-        <div class="option">
-        <p align="center">não tem uma conta?<a href="#">Registre-se</a></p>
-        </div>
-        </div>
+                <br>
+                <div class="option">
+                    <p align="center">não tem uma conta?<a href="#">Registre-se</a></p>
+                </div>
+    </div>
 </body>
 </html>
 
