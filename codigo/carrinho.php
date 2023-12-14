b398be86d5985a5e7c3e1df033c8400ba347b6bc
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-12-14 12:06:25 -0300

    wer

--- codigo/carrinho.php
+++ codigo/carrinho.php
@@ -1,52 +1,111 @@
-<?php
-include('header.php')
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="carrinho.css">
 
-<link rel="stylesheet" href="carrinho.css">
-
+</head>
 <body>
 
-<div class="title"> 
-        <h1 id="title-a">Carrinho de compras:<h1>
-    </div>
 
-    <div class='main-div'>
 
-        <div class='sub-main'>
-            <a href='#'>
-            <div class='products'>
-            <img src='../img/Monoprice_Select_Mini_V2.jpeg'>
-            <p>Monoprince Select Mini V2</p>
-            </div>
-            </a>
 
-            <a href='#'>
-            <div class='products'>
-            <img src='../img/Creality Ender 3 V2.jpeg'>
-            <p>3D Creality Ender 3 V2</p>
-            </div>
-            </a>
 
-            <a href='#'>
-            <div class='products'>
-            <img src='../img/Dremel_Digilab_3D40_3D_Printer.jpeg'>
-            <p>Impressora 3D Dremel Digilab 3D40 3D Printer - EDU Bundle</p>
-            </div>
-            </a>
-        </div>
-        <div class='sub-main'>
-            <a href='#'>
-            <div class='products'>
-            <img src='../img/Anycubic i3 Mega.jpeg'>
-            <p>Anycubic i3 Mega</p>
-            </div>
-            </a>
-        </div>
-
-    </div>
-</body>
-</html>
 
 <?php
-include('footer.php')
+include("header.php")
 ?>
+
+		<br>
+		<h1 align="center">CARRINHO DE COMPRAS</h1>
+		<br>
+		<div id="fundo">
+		<div id="itens"> </div>
+		<br>
+		<div>Total: <span id="total"></span> </div>
+
+		<script>
+			var total = 0; // variável que retorna o total dos produtos que estão na LocalStorage.
+			var i = 0;     // variável que irá percorrer as posições
+			var valor = 0; // variável que irá receber o preço do produto convertido em Float.
+	
+			// exibe o total dos recheios
+			document.getElementById("total").innerHTML = total.toFixed(2);
+	
+			for (i = 1; i <= 99; i++) {
+				var prod = localStorage.getItem("produto" + i);
+				if (prod != null) {
+					// Cria um elemento de div para cada item do carrinho
+					var itemDiv = document.createElement("div");
+	
+					// Exibe os dados do item dentro da div
+					itemDiv.innerHTML += localStorage.getItem("qtd" + i) + " x ";
+					itemDiv.innerHTML += localStorage.getItem("produto" + i);
+					itemDiv.innerHTML += " ";
+					itemDiv.innerHTML += "R$: " + localStorage.getItem("valor" + i);
+	
+					// Cria um botão de excluir para cada item
+					var excluirButton = document.createElement("button");
+					excluirButton.innerHTML = "Excluir";
+					excluirButton.addEventListener("click", createExcluirItemHandler(i));
+	
+					// Adiciona o botão de excluir à div do item
+					itemDiv.appendChild(excluirButton);
+	
+					// Adiciona a div do item à div "itens"
+					document.getElementById("itens").appendChild(itemDiv);
+	
+					valor = parseFloat(localStorage.getItem("valor" + i));
+					total = total + valor;
+				}
+			}
+			document.getElementById("total").innerHTML = total.toFixed(2);
+	
+			function createExcluirItemHandler(index) {
+				return function() {
+					// Lógica para excluir o item do carrinho
+					// Pode ser implementada aqui ou chamando uma função externa
+	
+					// Exemplo de código para remover o item atual do carrinho
+					this.parentNode.remove();
+	
+					// Atualizar o total
+					var itemValue = parseFloat(localStorage.getItem("valor" + index));
+					total -= itemValue;
+					document.getElementById("total").innerHTML = total.toFixed(2);
+	
+					// Remover o item da localStorage
+					localStorage.removeItem("produto" + index);
+					localStorage.removeItem("qtd" + index);
+					localStorage.removeItem("valor" + index);
+				}
+			}
+		</script>
+<br>
+		<button type="button" id="limpar" onclick=" localStorage.clear(); location.reload();"> Limpar carrinho </button>
+	</div>
+
+		<section>
+        <br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			
+			<div align="center" id="botaofinal"><a href="finalizar.html">FINALIZAR COMPRA</a></div>
+			
+			
+			
+
+		</section>
+		
+	<?php
+    include("footer.php")
+    ?>
